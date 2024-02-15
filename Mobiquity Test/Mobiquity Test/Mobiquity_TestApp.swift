118048b0eff91b1aa7abbe998e8fe86f97919f02
Ross Viviani <rossco_mt_@hotmail.com>
2024-02-15 16:54:12 +0000

    Initial commit

---
+++ Mobiquity Test/Mobiquity Test/Mobiquity_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mobiquity_TestApp.swift
+//  Mobiquity Test
+//
+//  Created by Ross Viviani on 15/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct Mobiquity_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
