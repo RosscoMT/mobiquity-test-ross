ebea835a83694b6fcc966609785366869c3e6640
Ross Viviani <rossco_mt_@hotmail.com>
2024-02-17 17:52:40 +0000

    Add the service request enum

---
+++ Shared/Sources/Shared/ServiceRequest.swift
@@ -0,0 +1,12 @@
+//
+//  ServiceRequest.swift
+//
+//
+//  Created by Ross Viviani on 17/02/2024.
+//
+
+import Foundation
+
+enum ServiceRequest: String {
+    case photoSearch = "flickr.photos.search"
+}
