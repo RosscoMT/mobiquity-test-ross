4f8c4bf7402847e1e1603145a332c0e94c07c906
Ross Viviani <rossco_mt_@hotmail.com>
2024-02-17 17:35:34 +0000

    Add the history view models

---
+++ Mobiquity Test/Mobiquity Test/Views/Model/HistoryDataModel.swift
@@ -0,0 +1,13 @@
+//
+//  HistoryDataModel.swift
+//  Mobiquity Test
+//
+//  Created by Ross Viviani on 15/02/2024.
+//
+
+import Foundation
+
+struct HistoryDataModel: Identifiable {
+    let id: UUID = UUID()
+    let title: String
+}
---
+++ Mobiquity Test/Mobiquity Test/Views/Model/HistoryViewDataModel.swift
@@ -0,0 +1,12 @@
+//
+//  HistoryViewDataModel.swift
+//  Mobiquity Test
+//
+//  Created by Ross Viviani on 17/02/2024.
+//
+
+import Foundation
+
+struct HistoryViewDataModel {
+    static var history: [HistoryDataModel] = []
+}
