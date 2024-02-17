8cf5cb70999557461240b4fbea2ffee64a294438
Ross Viviani <rossco_mt_@hotmail.com>
2024-02-17 17:32:28 +0000

    Add custom master view

--- Shared/Sources/Shared/Shared.swift
+++ Shared/Sources/Shared/Shared.swift
@@ -1,2 +1,16 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+import SwiftUI
+
+public struct Shared<Content: View>: View {
+    
+    @ViewBuilder let content: Content
+    
+    public init(view: Content) {
+        content = view
+    }
+    
+    public var body: some View {
+        content
+    }
+}
