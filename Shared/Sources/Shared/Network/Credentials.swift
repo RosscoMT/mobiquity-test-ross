939748bb0aa0573d307fd8c9f8c9685fad086053
Ross Viviani <rossco_mt_@hotmail.com>
2024-02-19 17:08:58 +0000

    Add a Credentials enum for the api key

---
+++ Shared/Sources/Shared/Network/Credentials.swift
@@ -0,0 +1,13 @@
+//
+//  Credentials.swift
+//
+//
+//  Created by Ross Viviani on 19/02/2024.
+//
+
+import Foundation
+
+///Storage of the
+enum Credentials {
+    static var apiKey = "855c1aec21159ada77f577284f483c20"
+}
